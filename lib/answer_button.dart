b138cbf4c5e3bddc07a74b9d5e2bda2388251639
fominmaksim <fomin.max.13@gmail.com>
2024-08-07 19:20:04 +0200

    feat: added first question page

---
+++ lib/answer_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        backgroundColor: const Color.fromARGB(255, 1, 92, 99),
+        foregroundColor: Colors.white,
+      ),
+      onPressed: onTap,
+      child: Text(
+        answerText,
+      ),
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/models/quiz_question.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -9,8 +12,29 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final QuizQuestion currentQuestion = questions[0];
+
   @override
   Widget build(context) {
-    return const Text('Hey');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 40),
+          ...currentQuestion.answers.map(
+            (item) {
+              return AnswerButton(answerText: item, onTap: () {});
+            },
+          )
+        ],
+      ),
+    );
   }
 }
